4eb14ce698f8c474539177f928063a58124845a3
maximusprimor <fabio.zinato@gmail.com>
2023-02-27 13:42:01 -0300

    Correcao rede

--- Terraform/azure-windows-instance/main.tf
+++ Terraform/azure-windows-instance/main.tf
@@ -1,22 +1,43 @@
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = var.vm_name
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  size                  = var.vm_size
-  admin_username        = "eadadm"
-  admin_password        = "Fab12345"
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  computer_name         = var.vm_name
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.vm_name}-vnet"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.vm_name}-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.vm_name}-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "allow-winrm"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5986"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
-  os_disk {
-    name              = "${var.vm_name}-osdisk"
-    caching           = "ReadWrite"
-    storage_account_type = var.disk_type
+  security_rule {
+    name                       = "allow-icmp"
+    priority                   = 3000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
@@ -32,42 +53,39 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.vm_name}-vnet"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.vm_name}-subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_network_interface_security_group_association" "isga" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_public_ip" "vm_public_ip" {
   name                = "${var.vm_name}-pubip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_security_group" "win_remote_access" {
-  name                = "example-nsg"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  size                  = var.vm_size
+  admin_username        = "eadadm"
+  admin_password        = "Fab12345"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  computer_name         = var.vm_name
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
 
-  security_rule {
-    name                       = "allow-winrm"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "5986"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  os_disk {
+    name              = "${var.vm_name}-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = var.disk_type
   }
 }
+
 
