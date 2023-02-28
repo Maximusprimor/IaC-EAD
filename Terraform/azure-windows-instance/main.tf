e8c5038aa92c9ab515ed8fd652ee18915e0c1bd9
maximusprimor <fabio.zinato@gmail.com>
2023-02-28 07:18:35 -0300

    Correcao IP pub

--- Terraform/azure-windows-instance/main.tf
+++ Terraform/azure-windows-instance/main.tf
@@ -58,7 +58,7 @@ resource "azurerm_public_ip" "vm_public_ip" {
   name                = "${var.vm_name}-pubip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dinamic"
+  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "nic" {
